ffb706297c89d85ef7353ef692b3d7b7c82c6202
mkhit <gor.mhitaryan@list.ru>
2019-09-14 16:11:09 +0300

    Application skeleton

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,17 +4,11 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
-use App\Entity\Article;
 
 class DefaultController extends AbstractController
 {
-    public function index(EntityManagerInterface $em)
+    public function index()
     {
-        $repository = $em->getRepository(Article::class);
-        $articles = $repository->findAll();
-
-        return $this->render('index/index-1.html.twig', [
-            'articles' => $articles,
-        ]);
+        return new Response("Hello!");
     }
 }
